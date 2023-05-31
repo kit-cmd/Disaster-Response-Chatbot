de184b296b1f81e49a45c4218e83bf1381cbf81a
Daki404 <daki403.1@gmail.com>
2023-05-31 09:16:13 +0900

    Refactor: scrap_safekorea.py get urls
    scrap_data/scrap_safekorea.py get urls and save to chat_bot/urls.txt

--- scrap_data/scrap_safekorea.py
+++ scrap_data/scrap_safekorea.py
@@ -6,7 +6,6 @@ def render_url(value:str = "01"):
     return f"http://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/contents/prevent/prevent{value}.html?menuSeq=126'"
 
 
-
 # Send a GET request to the website
 response = requests.get(render_url())
 
@@ -17,8 +16,9 @@ soup = BeautifulSoup(response.content, 'html.parser')
 elements = soup.find_all(class_='links_listCell')
 
 # Extract the onclick phrase from the element
-for i in elements:
-    idx = i.a['onclick'].rfind('.')
-    val = i.a['onclick'][idx-2:idx]
-    
-    print(render_url(val))
+with open("./chat_bot/urls.txt", "w") as f:
+    for i in elements:
+        idx = i.a['onclick'].rfind('.')
+        val = i.a['onclick'][idx-2:idx]
+        
+        f.write(render_url(val) + "\n")
