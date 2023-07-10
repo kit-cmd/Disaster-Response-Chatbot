3a7c069d8be9e734a1bf4bbd8d8e69aadd6937f7
bini59 <myuni0234@gmail.com>
2023-07-10 22:43:21 +0900

    feat : pwa 템플릿 적용
    기존것 수정해서 구현하려다가 템플릿이 있다는것을 알았습니다..
    - create-react-app --template pwa-typescript

---
+++ cmd-pwa/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Main, Info, Mypage, Account, Realtime, Predict } from './pages';
+import { Sidebar } from './components/Sidebar';
+
+import './styles/App.css';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/predict" element={<Predict />} />
+          <Route path="/realtime" element={<Realtime />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ cmd-pwa/src/pages/Mypage/Mypage.tsx
@@ -0,0 +1,5 @@
+export const Mypage = () => {
+    return (
+        <div>마이페이지 입니다</div>
+    )
+}
