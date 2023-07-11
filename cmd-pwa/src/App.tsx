e2beb41334b1c7c56d03e4b078852213a414d5cc
bini59 <myuni0234@gmail.com>
2023-07-12 08:45:58 +0900

    feat : FCM 이용한 알림 구현 완료
    - 서버와 통신 필요, 서버 통신 방법은 14~15즈음 논의
    - firebase api 구현 완료
    - .env.development.local 파일 추가
        확인 후 전달 예정

--- cmd-pwa/src/App.tsx
+++ cmd-pwa/src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Main, Info, Mypage, Account, Realtime, Predict } from './pages';
 import { Sidebar } from './components/Sidebar';
 
+
+import "./Firebase"
+
 import './styles/App.css';
 
-function App() {
+const App = () => {
+
+  // inside the jsx being returned:
+  
+
+  const RequestNotificationPermission = () => {
+    Notification.requestPermission().then(function (result) {
+      if (result === 'granted') {
+        console.log('알림이 허용되었습니다.');
+      } else if (result === 'denied') {
+        console.log('알림이 차단되었습니다.');
+      }
+    });
+  }
+
+  useEffect(() => {
+    RequestNotificationPermission();
+  }, []);
+
   return (
     <>
       <Router>
