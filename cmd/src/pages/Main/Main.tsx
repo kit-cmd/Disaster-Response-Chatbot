415020244e500880373a3bbf114ea36255dc1ef5
bini59 <myuni0234@gmail.com>
2023-07-12 11:52:59 +0900

    feat : 최신 재난 정보 구현
    - 틀 제작 완료
    - style 같이 적용완료
    - api 연결 필요

--- cmd/src/pages/Main/Main.tsx
+++ cmd/src/pages/Main/Main.tsx
@@ -2,11 +2,19 @@ import { useNavigate } from 'react-router-dom';
 
 import "./index.css"
 import { CalamityInfo } from './Components/CalamityInfo';
+import { RecentInfo } from './Components/RecentInfo';
 
 export const Main = () => {
     const navigate = useNavigate();
+    
 
-
+    const recentInfos = [
+        "재난 최신 정보1",
+        "재난 최신 정보2",
+        "재난 최신 정보3",
+        "재난 최신 정보4",
+        "재난 최신 정보5",
+    ]
     return (
         <section className='container'>
             <section className='calamity-info-section'>
@@ -28,6 +36,10 @@ export const Main = () => {
             <section className='calamity-today-tip'>
                 <div>오늘의 팁</div>
                 <div>{`사람들에게 도움이 될만한 오늘의 팁 표시`}</div>
+            </section>
+
+            <section className='calamity-recent-section'>
+                <RecentInfo infos={recentInfos} />           
             </section>
         </section>
         
