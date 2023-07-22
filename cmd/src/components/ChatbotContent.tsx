c617555e1bd7bfa97a4c5a9d68546416f4bda845
bini59 <myuni0234@gmail.com>
2023-07-22 15:21:16 +0900

    feat : 챗봇 연결완료

--- cmd/src/components/ChatbotContent.tsx
+++ cmd/src/components/ChatbotContent.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface ChatbotContentProps {
     setInActive: () => void
@@ -19,14 +19,67 @@ export const ChatbotContent = ({ setInActive }: ChatbotContentProps) => {
         isUser: false
     }
 
+    const endpoint = "ws://localhost:8000/ws"
+    const ws = useRef<WebSocket | null>(null);
+
+    const handleStreamMessage = async (message: string) => {
+        return new Promise<void>((resolve) => {
+          setTimeout(() => {
+            setChats((prevChats) => {
+              const lastChat = prevChats[prevChats.length - 1];
+              if (lastChat && !lastChat.isUser) {
+                // If the last chat is from the bot, append the message
+                lastChat.text += message;
+                return [...prevChats];
+              } else {
+                // Otherwise, add a new chat for the stream message
+                const newChat = { text: message, isUser: false };
+                return [...prevChats, newChat];
+              }
+            });
+            resolve();
+          }, 1000);
+        });
+    };
+      
+    useEffect(() => {
+        ws.current = new WebSocket(endpoint);
+        ws.current.onopen = () => {
+            console.log("ws opened");
+        };
+        ws.current.onclose = () => {
+            console.log("ws closed");
+        };
+        ws.current.onerror = (err) => {
+            console.log("ws error", err);
+        };
+        ws.current.onmessage = async (msg) => {
+            console.log("ws message", msg);
+            var data = JSON.parse(msg.data);
+            if (data.sender === "bot") {
+                if (data.message_type === "start") {
+                    const newChat = { text: data.message, isUser: false }
+                    setChats((prevChats) => [...prevChats, newChat]);
+                }
+                if (data.message_type === "stream") {
+                    await handleStreamMessage(data.message);
+                }
+            }
+        };
+        return () => {
+            ws.current?.close();
+        };
+    }, []);
+
     const handleInput = (e: any) => {
         setInput(e.target.value)
     }
 
     const handleKeyPress = (e: any) => {
         if (e.key === "Enter") {
+            ws.current?.send(input);
             const newChat = { ...chat, text: input, isUser: true }
-            setChats([...chats, newChat])
+            setChats((prevChats) => [...prevChats, newChat]);
             setInput("")
         }
     }
