39ea9e0fc3946170f2d5f9c2711a1237abc7b862
JaeHyunGround <gothddlek@naver.com>
2023-07-30 00:25:20 +0900

    feat : login 병합

--- cmd/src/pages/Info/Info.tsx
+++ cmd/src/pages/Info/Info.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import "../../styles/Info.css"
+
 export const Info = () => {
+
+    const infos = [
+        {
+            name : "태풍",
+            content : "태풍 정보"
+        },
+        {
+            name : "호우",
+            content : "호우 정보"
+        }, 
+        {
+            name : "지진",
+            content : "지진 정보"
+        }, 
+        {
+            name : "대설",
+            content : "대설 정보"
+        }, 
+    ];
+
+    const [tapActive, setTapActive] = useState(0)
+
+    const InfoTapHandler = (e : any) => {
+        setTapActive(e.currentTarget.id);
+    }
+
     return (
-        <div>재난 정보 제공 페이지 입니다</div>
+        <div className="info_container">
+            <div className="info_header">
+                <p>안전지침 정보</p>
+            </div>
+            <div className="infos">
+                <div className="info_tap">
+                    <div id="0" className={(tapActive == 0) ? "info_tap_active" : "info_tap_none"} onClick={InfoTapHandler}>태풍</div>
+                    <div id="1" className={(tapActive == 1) ? "info_tap_active" : "info_tap_none"} onClick={InfoTapHandler}>호우</div>
+                    <div id="2" className={(tapActive == 2) ? "info_tap_active" : "info_tap_none"} onClick={InfoTapHandler}>지진</div>
+                    <div id="3" className={(tapActive == 3) ? "info_tap_active" : "info_tap_none"} onClick={InfoTapHandler}>대설</div>
+                </div>
+                <div className="info_text">{infos[tapActive].content}</div>
+            </div>
+        </div>
     )
 }
