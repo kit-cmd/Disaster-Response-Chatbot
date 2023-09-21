31d9e721deff7d30749ff6a7b2e612b3cb4cfdb0
bini59 <myuni0234@gmail.com>
2023-09-21 09:57:35 +0900

    style : 실시간 재난정보 스타일 변경

--- cmd-pwa/src/components/Calamity.tsx
+++ cmd-pwa/src/components/Calamity.tsx
@@ -2,6 +2,14 @@ import { useEffect, useState } from "react";
 import { Calamity as calamity } from "../types/calamity";
 import axios from "axios";
 
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import { CardMedia } from "@mui/material";
+
+
 interface CalamityProps {
     calamity: calamity;
     type: string | null;
@@ -30,32 +38,62 @@ export const Calamity = (props: CalamityProps) => {
             })
     }, [])
 
-
+    const sx = 
+        type !== null
+        ? { width: '90%', height: 350, margin: '15px 5%' }
+        : { width: '40%', height: '35vh', minWidth: 300, margin: '0 5%' }
+    
+    
+    const mapimg = 
+        `https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=${locate?.x},${locate?.y}&level=11&X-NCP-APIGW-API-KEY-ID=${api_id}`
+    
+    
     return (
-        <section className={type !== null ? "calamity-summary-wrapper" + "-" + type : "calamity-summary-wrapper" }>
-            <div className="calamity-summary-header">
-                {calamity.name}
-                {/* 재난 위치로 변경할 것 */}
-            </div>
-            <div className="calamity-summary-body">
-                <div className="calamity-summary-map-wrapper">
-                    <div>
-                        {
-                            locate === null
-                                ? "이미지 로드중"
-                                : <img src={`https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=${locate?.x},${locate?.y}&level=11&X-NCP-APIGW-API-KEY-ID=${api_id}`} />
-                        }
-                        {/* 네이버 지도 api사용해서 지도 연결 */}
-                        {/* 위,경도 받아오는 API 구성할 것 */}
-                    </div>
-                </div>
-
-                <div className="calamity-summary-info-wrapper">
-                    <div>재난 종류 : {calamity.name}</div>
-                    <div>상세 정보 : {calamity.description}</div>
-                    <div>발생 시각 : {calamity.date}</div>
-                </div>
-            </div>
-        </section>
+        <Card sx={sx}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    {calamity.name}
+                </Typography>
+                <Typography component="div">
+                    {calamity.description}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {calamity.date}
+                </Typography>
+            </CardContent>
+            <CardMedia 
+                component="img"
+                sx={{ width: '100%', height: 240 }}
+                image={mapimg}
+                alt="이미지 로드중"
+            />
+        </Card>
     )
+    // return (
+    //     <section className={type !== null ? "calamity-summary-wrapper" + "-" + type : "calamity-summary-wrapper" }>
+    //         <div className="calamity-summary-header">
+    //             {calamity.name}
+    //             {/* 재난 위치로 변경할 것 */}
+    //         </div>
+    //         <div className="calamity-summary-body">
+    //             <div className="calamity-summary-map-wrapper">
+    //                 <div>
+    //                     {
+    //                         locate === null
+    //                             ? "이미지 로드중"
+    //                             : <img src={`https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=${locate?.x},${locate?.y}&level=11&X-NCP-APIGW-API-KEY-ID=${api_id}`} />
+    //                     }
+    //                     {/* 네이버 지도 api사용해서 지도 연결 */}
+    //                     {/* 위,경도 받아오는 API 구성할 것 */}
+    //                 </div>
+    //             </div>
+
+    //             <div className="calamity-summary-info-wrapper">
+    //                 <div>재난 종류 : {calamity.name}</div>
+    //                 <div>상세 정보 : {calamity.description}</div>
+    //                 <div>발생 시각 : {calamity.date}</div>
+    //             </div>
+    //         </div>
+    //     </section>
+    // )
 }
